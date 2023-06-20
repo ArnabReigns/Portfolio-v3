9f3b38480ae71ba53129e9cafda9ca8dc94e0cf6
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2023-06-20 10:54:25 +0530

    Added A Footer

--- src/page/Home/sections/Footer.jsx
+++ src/page/Home/sections/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
-  )
-}
+    <div className="Footer">
+      <div className="content">
+        <h3>What's Next?</h3>
+        <h2>GET IN TOUCH</h2>
+        <p>
+          Although I’m not currently looking for any new opportunities, my inbox
+          is always open. Whether you have a question or just want to say hi,
+          I’ll try my best to get back to you!
+        </p>
+        <button onClick={()=>window.open('mailto:arnabchatterjee912@gmail.com')}>Say Hello</button>
+      </div>
+      <div className="sub">
 
-export default Footer
+      <p>Designed and Built by Arnab Chatterjee</p>
+      <p>2023</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
